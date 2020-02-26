5846e4a1bbc615252856f5a2a8a4d3e01e55d557
Roman Kindler <roman.kindler.1997@googlemail.com>
2020-02-26 14:41:23 +0100

    SystemUtils hinzugefuegt

---
+++ src/utils/system/SystemUtils.java
@@ -0,0 +1,111 @@
+package utils.system;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class SystemUtils {
+	public static final String JRE_HOME_DIRECTORY_PROPERTY_NAME = "java.home";
+	public static final String JRE_LIBRARY_PATH_PROPERTY_NAME = "java.library.path";
+	public static final String JRE_CLASS_PATH_PROPERTY_NAME = "java.class.path";
+	public static final String JRE_EXTENSION_LIBRARY_PATHS_PROPERTY_NAME = "java.ext.dirs";
+	public static final String JDK_VERSION_PROPERTY_NAME = "java.version";
+	public static final String JRE_VERSION_PROPERTY_NAME = "java.runtime.version";
+	public static final String FILE_SEPARATOR_PROPERTY_NAME = "file.separator";
+	public static final String PATH_SEPARATOR_PROPERTY_NAME = "path.separator";
+	public static final String LINE_SEPARATOR_PROPERTY_NAME = "line.separator";
+	public static final String USER_NAME_PROPERTY_NAME = "user.name";
+	public static final String USER_HOME_DIRECTORY_PROPERTY_NAME = "user.home";
+	public static final String USER_CURRENT_DIRECTORY_PROPERTY_NAME = "user.dir";
+	public static final String OS_NAME_PROPERTY_NAME = "os.name";
+	public static final String OS_VERSION_PROPERTY_NAME = "os.version";
+	public static final String OS_ARCHITECTURE_PROPERTY_NAME = "os.arch";
+	
+	public static String getPropertyValue(String propertyName) {
+		try {
+			return System.getProperty(propertyName);
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	
+	public static String setPropertyValue(String propertyName, String propertyValue) {
+		try {
+			return System.setProperty(propertyName, propertyValue);
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	
+	public static String getJreHomeDirectory() {
+		return getPropertyValue(JRE_HOME_DIRECTORY_PROPERTY_NAME);
+	}
+	
+	public static String getJreLibraryPath() {
+		return getPropertyValue(JRE_LIBRARY_PATH_PROPERTY_NAME);
+	}
+	
+	public static String getJreClassPath() {
+		return getPropertyValue(JRE_CLASS_PATH_PROPERTY_NAME);
+	}
+	
+	public static String getJreExtensionLibraryPaths() {
+		return getPropertyValue(JRE_EXTENSION_LIBRARY_PATHS_PROPERTY_NAME);
+	}
+	
+	public static String getJdkVersion() {
+		return getPropertyValue(JDK_VERSION_PROPERTY_NAME);
+	}
+	
+	public static String getJreVersion() {
+		return getPropertyValue(JRE_VERSION_PROPERTY_NAME);
+	}
+	
+	public static String getFileSeparator() {
+		return getPropertyValue(FILE_SEPARATOR_PROPERTY_NAME);
+	}
+	
+	public static String getPathSeparator() {
+		return getPropertyValue(PATH_SEPARATOR_PROPERTY_NAME);
+	}
+	
+	public static String getLineSeparator() {
+		return getPropertyValue(LINE_SEPARATOR_PROPERTY_NAME);
+	}
+	
+	public static String getUserName() {
+		return getPropertyValue(USER_NAME_PROPERTY_NAME);
+	}
+	
+	public static String getUserHomeDirectory() {
+		return getPropertyValue(USER_HOME_DIRECTORY_PROPERTY_NAME);
+	}
+	
+	public static String getUserCurrentDirectory() {
+		return getPropertyValue(USER_CURRENT_DIRECTORY_PROPERTY_NAME);
+	}
+	
+	public static String getOsName() {
+		return getPropertyValue(OS_NAME_PROPERTY_NAME);
+	}
+	
+	public static String getOsVersion() {
+		return getPropertyValue(OS_VERSION_PROPERTY_NAME);
+	}
+	
+	public static String getOsArchitecture() {
+		return getPropertyValue(OS_ARCHITECTURE_PROPERTY_NAME);
+	}
+	
+	public static InetAddress getLocalInetAddress() {
+		try(Socket s = new Socket()) {
+			s.connect(new InetSocketAddress("google.com", 80));
+			return s.getLocalAddress();
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+}
