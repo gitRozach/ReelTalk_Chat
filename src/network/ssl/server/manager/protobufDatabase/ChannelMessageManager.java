b88c6772a58d3fa94c9455729ef529f4d6c92916
Roman Kindler <roman.kindler.1997@googlemail.com>
2020-03-10 13:29:59 +0100

    ChannelMessageManager Erweiterung
    Anfang Ausbau ChannelMessageManager

--- src/network/ssl/server/manager/protobufDatabase/ChannelMessageManager.java
+++ src/network/ssl/server/manager/protobufDatabase/ChannelMessageManager.java
@@ -2,10 +2,16 @@ package network.ssl.server.manager.protobufDatabase;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import protobuf.ClientChannels.ChannelBase;
 import protobuf.ClientMessages.ChannelMessage;
 
 public class ChannelMessageManager extends ProtobufFileDatabase<ChannelMessage>{	
+	protected Map<ChannelBase, ProtobufFileDatabase<ChannelMessage>> messageDatabases;
+	
 	public ChannelMessageManager(String databaseFilePath) throws IOException {
 		this(new File(databaseFilePath));
 	}
@@ -14,5 +20,12 @@ public class ChannelMessageManager extends ProtobufFileDatabase<ChannelMessage>{
 		super(ChannelMessage.class, databaseFile);
 	}
 	
+	public List<ChannelMessage> getMessagesFromByChannelId(int channelId){
+		List<ChannelMessage> resultList = new ArrayList<>();
+		
+		
+		
+		return resultList;
+	}
 	
 }
